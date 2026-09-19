7f3cb9c31f01edecb29fb2040c27c675402661d2
surprise2024-cpu <surprise2024@gmail.com>
2026-09-19 22:20:29 +0200

    feat: add validateBookUpdate middleware for validating book updates

---
+++ src/middleware/validateBookUpdate.ts
@@ -0,0 +1,38 @@
+import type { Request, Response, NextFunction } from "express";
+
+// creates and exports the middleware function
+export const validateBookUpdate = (
+
+    req: Request,
+    res: Response,
+    next: NextFunction // tells express to continue to the next middleware or route
+
+) => {
+
+    const { title, year, authorId } = req.body;
+
+    if (!title || typeof title !== 'string' || title.trim()  === '') {
+        return res.status(400).json({
+            success: false,
+            error: 'Book title is required'
+        });
+    }
+
+    if (year !== undefined || typeof year !== 'number') {
+        return res.status(400).json({
+            success: false,
+            error: 'Book year must be a number'
+        });
+    }
+
+    if (authorId === undefined || typeof authorId !== 'number') {
+        return res.status(400).json({
+            success: false,
+            error: 'authorId must be a number'
+        });
+    }
+
+    // makes the request continue to the actual route
+    next();
+
+};
