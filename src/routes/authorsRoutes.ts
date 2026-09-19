f978d7651721b64b7a9f10b9d910f2059a973fa0
surprise2024-cpu <surprise2024@gmail.com>
2026-09-19 19:48:49 +0200

    feat: implement authors post route for adding new authors

---
+++ src/routes/authorsRoutes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { authors } from "../data/store.js";
+
+// creating a new router object
+const router = Router();
+
+// creating the post route
+router.post('/', (req, res) => {
+
+    // gets the name from the JSON body
+    const { name } = req.body;
+
+    // new object representing an author
+    const newAuthor = {
+        id: authors.length + 1,
+        name
+    };
+
+    // adds the new author to the end of the authors array
+    authors.push(newAuthor);
+
+    // sends a status 201 response which basicaly means the object was successfully created
+    res.status(201).json({
+        success: true,
+        data: newAuthor
+    });
+});
+
+// makes the router available to use all over the app
+export default router;
