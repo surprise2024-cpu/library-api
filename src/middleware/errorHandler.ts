54eb1622a08cd218c01a93824851c61f386dfa72
surprise2024-cpu <surprise2024@gmail.com>
2026-09-20 03:35:58 +0200

    feat: enhance error handling middleware to support optional status codes

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,14 +1,25 @@
 import type { Request, Response, NextFunction } from "express";
 
+// allows an optional status 
+type AppError = Error & {
+    status?: number;
+};
 
 export const errorHandler = (
-    err: Error, // tells express that this is an error-handling middleware
+    err: AppError, // tells express that this is an error-handling middleware
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
 
     console.error(err.message);
+
+    if (err.status === 400) {
+        return res.status(400).json({
+            success: false,
+            error: 'Invalid JSON'
+        });
+    }
 
     res.status(500).json({
         success: false,
