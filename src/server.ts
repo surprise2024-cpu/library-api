0daea77ec9221876e42757c9f69837585ff54663
surprise2024-cpu <surprise2024@gmail.com>
2026-09-20 05:23:06 +0200

    feat: reorganize server setup and add notFound middleware for improved error handling

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import authorsRoutes from './routes/authorsRoutes.js'
 import { logger } from './middleware/logger.js';
 import booksRoutes from './routes/booksRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { notFound } from './middleware/notFound.js';
 
 // creating the express app
 const app = express();
@@ -10,20 +11,11 @@ const app = express();
 // the port the server is running on
 const PORT = 4000;
 
-// allow our express server to understand JSON requests
+// allows our express server to understand JSON requests
 app.use(express.json());
 
 // tells express to run the logger middleware for every incoming request
 app.use(logger);
-
-// makes each route inside authorsRoutes.ts start with (/authors)
-app.use('/authors', authorsRoutes);
-
-// makes each route inside booksRoutes.ts start with (/books)
-app.use('/books', booksRoutes);
-
-// gives the app access to centralized error handlers
-app.use(errorHandler);
 
 // creates the apps first route
 app.get('/', (req, res) => {
@@ -31,6 +23,18 @@ app.get('/', (req, res) => {
         message: 'Library API is running'
     });
 });
+
+// makes each route inside authorsRoutes.ts start with (/authors)
+app.use('/authors', authorsRoutes);
+
+// makes each route inside booksRoutes.ts start with (/books)
+app.use('/books', booksRoutes);
+
+// gives the app a response to give for routes that are not within the apps specifics
+app.use(notFound);
+
+// gives the app access to centralized error handlers
+app.use(errorHandler);
 
 // this actually starts the server.
 app.listen(PORT, () => {
